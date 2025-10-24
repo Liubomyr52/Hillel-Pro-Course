327554010465d9135c58250e29503264f5ad9ba1
Liubomyr H <liubomirguzhumit@gmail.com>
2025-10-24 16:29:19 -0400

    lesso_1

---
+++ script.js
@@ -0,0 +1,161 @@
+// var name = 'Liubomyr'
+// var lastName = 'Huzhumit'
+// var age = '13'
+
+
+// let name = 'Liubomyr'
+// console.log('name1',  name)
+// name = 'Dima'
+// console.log('name2',  name)
+
+
+// const age = 30
+// age = 67
+// console.log('age', age)
+
+
+//string
+// const name = 'Dima'
+
+// //Number
+// const age = 22
+
+
+
+// //boolean
+// const isStudent = true
+// const isLegal = false
+
+
+
+
+// EXAMPLE: boolean
+// const isStudent = false
+// if (isStudent) {
+//     console.log('Hello, Student!')
+// } else {
+//     console.log('You suck!')
+// }
+
+
+
+// const uniqueID = Symbol('description!');
+// console.log(uniqueID)
+
+
+
+// let person = {
+//     name: 'Alex',
+//     age:  32,
+//     gender:  'Male'
+// }
+
+// person.lastName = 'Huzhumit'
+// person.age = '45'
+
+// console.log(person)
+// // person = {
+// //     lastName: 'Huzhumit'
+// // }
+// console.log(person.name)
+
+
+
+//if in name Dima you put '' the output will be false
+//This changes String(Dima) to Boolean!
+// const name = 'Dima'
+// const isName = Boolean(name)
+// console.log(isName)
+
+
+
+// //This changes Number(30) to String!
+// const age = 30
+// const ageToString = String(age)
+// console.log(ageToString)
+
+
+
+// This changes null to boolean!
+// const name1 = null
+// const isName1 = Boolean(name1)
+// console.log(isName1)
+
+
+
+
+// let num = 42
+// let str = "The answear is" + num
+// console.log(str)
+
+
+
+// let x = 10
+// let y = 67
+// let isLess = x < y
+// console.log(isLess)
+
+
+
+// const admin = {
+//     age: 19,
+//     name: 'Alex',
+//     race: 'white',
+//     gender: 'male'
+// }
+
+// const barmen = {
+//     age: 22,
+//     name: 'John',
+//     race: 'black',
+//     gender: 'male'
+// }
+
+// const legalAge = 18
+
+// const isAdminLegal = admin.age >= legalAge 
+// console.log(isAdminLegal)
+
+
+
+// const name = 'John'
+// const age = 30
+
+// const message = `Hello, my name is ${name} and i am ${age}.`
+// console.log(message)
+
+
+
+
+
+
+
+// let x = 10
+// let y = 10
+// let isLessOrEqualTo = x <= y
+// console.log(isLessOrEqualTo)
+
+
+
+
+// let num = 42
+// let str = "42"
+// if (num == str) {
+//     console.log("Equal")
+// }
+// else {
+//     console.log("Not Equal")
+// }
+
+
+
+//YOU CAN DO IT EVEN WITH CONST BECAUSE WE CHANGE THE PERSON NOT THE CONST
+
+// const person = {
+//     name: 'Alex',
+//     age:  32,
+//     gender:  'Male'
+// }
+
+// person.lastName = 'Huzhumit'
+// console.log(person)
