909d692c4d8af370fcc89d4439150ace44dcb541
Liubomyr H <liubomirguzhumit@gmail.com>
2025-10-24 16:50:45 -0400

    homework-1.1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const user = typeof 'Course'
-const user1 = typeof 25
-const user2 = typeof true
+let user1 = prompt('what is your name?')
+let user2 = prompt('How old are you?')
+let user3 = prompt('What is your gender?')
 
-console.log(user)
-console.log(user1)
-console.log(user2)
+console.log(`${user1}, ${user2}, ${user3}`)
+
+ 
