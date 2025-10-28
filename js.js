a04f5c6c184d44c565e76f9d94896d70da96dd9d
Liubomyr H <liubomirguzhumit@gmail.com>
2025-10-28 19:39:43 -0400

    HomeWork lesson 4

---
+++ js.js
@@ -0,0 +1,68 @@
+//HOMEWORK 1:
+
+// const num = prompt('What is the username')
+
+// alert(`Hello, ${num}! How are you`)
+
+
+//HOMEWORK 2:
+
+
+// I didn't find a way to do thsi homework.
+
+
+
+
+
+//HOMEWORK 3:
+
+
+// const year = prompt('What year were you born?')
+// const city = prompt('What city do you live?')
+// const sport = prompt('What is your favorite sport?')
+
+// if(year == null || city == null || sport == null) {
+//   alert('You canceled')
+// }
+// else if(city == 'Kyiv') {
+//   alert(`${2025 - year}\nТи живеш у столиці України`)
+// }
+// else if(city == 'Washington') {
+//   alert(`${2025 - year}\nТи живеш у столиці США`)
+// }
+// else if(city == 'London') {
+//   alert(`${2025 - year}\nТи живеш у столиці Великої Британії`)
+// }
+// else {
+//   alert(`Ти живеш у місті ${city}`)
+// }
+
+
+
+
+//HOMEWORK 4:
+
+
+
+// let numOrStr = prompt('input number or string')
+// console.log(numOrStr)
+
+// switch (true) {
+//   case numOrStr === null:
+//     console.log('ви скасували')
+
+//     break;
+
+//     case numOrStr.trim() === '': 
+//       console.log('Empty String')
+    
+//       break;
+
+//   case isNaN(+numOrStr):
+//     console.log('number is Ba_NaN')
+
+//     break;
+
+//   default: 
+//       console.log('OK') 
+// }
