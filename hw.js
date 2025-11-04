1760a3db7b8e0d07198a6ef6bc5e31bf64dc5bc8
Liubomyr H <liubomirguzhumit@gmail.com>
2025-11-04 17:41:16 -0500

    homework-5

---
+++ hw.js
@@ -0,0 +1,53 @@
+// Homework 1;
+
+// for(let i = 20; i <= 30; i += 0.5) {
+//   console.log(i)
+// }
+
+
+
+
+// Homework 2;
+
+// let rate = 26
+
+// for(let i = 10; i <= 100; i += 10) {
+//   console.log('$', i, 'in', 'гривень')
+//   console.log('гривень', rate * i)
+// }
+
+
+
+
+// Homework 3;
+
+
+// let input = +prompt('Enter a number!')
+
+// for(let i = 1; i <= 100; i++) {
+//   if(i ** 2 <= input) {
+//     console.log(i)
+//   }
+// }
+
+
+
+// Homework 4;
+
+
+// let input = +prompt('Enter a number!')
+// let isPrime = true
+
+// if(input <= 1) {
+//   isPrime = false;
+// }
+
+// else {
+//   for(let i = 2; i < input; i++) {
+//     if(input % i === 0) {
+//       isPrime = false;
+//       break;
+//     }
+//   }
+// }
+// console.log(isPrime ? `${input} is a prime number.` : `${input} is not a prime number.`);
