7a77c39d67ecce3782c235e5f7736534eb669d47
Liubomyr H <liubomirguzhumit@gmail.com>
2025-11-24 11:12:57 -0500

    HOMEWORK

---
+++ hw-8.js
@@ -0,0 +1,58 @@
+// HOMEWORK: 2
+
+// function findTheProduct(x) {
+//   return function(y) {
+//     return x * y
+//   }
+// }
+// console.log(findTheProduct(5)(10))
+
+
+
+
+// HOMEWORK 3:
+
+
+
+// const input = +prompt('Enter a number greater than 100')
+// let lastNumber = 0
+
+// function process(input, lastNumber) {
+//   for(let i = 0; i < 10; i++) {
+//     lastNumber = input
+
+//   if(input <= 100) {
+//    input = +prompt('Enter the number again')
+//   }
+//   else {
+//     break
+//   }
+// }
+// console.log(lastNumber)
+// }
+// process(input, lastNumber)
+
+
+
+
+// HOMEWORK 1: NEED TO WORK ON IT;
+
+
+
+
+// function sum() {
+
+//   let summ = 0
+
+//   return function result(num) {
+//     summ += num
+//     return summ
+//   }
+// }
+// const a = sum()
+
+// console.log(a(5))
+// console.log(a(5))
+
+
+
